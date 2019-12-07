964fcae87799ab4c7c69f4a8a019cd345ce8001a
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2019-12-07 19:27:03 +0100

    initial commit

---
+++ src/main/java/com/server/calendarapp/CalendarappApplication.java
@@ -0,0 +1,13 @@
+package com.server.calendarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarappApplication.class, args);
+    }
+
+}
