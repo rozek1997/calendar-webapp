b410935c38dc06394636cab9c5f5c7ed34bc344a
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2019-12-16 15:46:53 +0100

    Add google integration

--- src/main/java/com/server/calendarapp/service/GoogleEventsService.java
+++ src/main/java/com/server/calendarapp/service/GoogleEventsService.java
@@ -6,16 +6,12 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.server.calendarapp.security.CustomerPrinciple;
-import jdk.jfr.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 public class GoogleEventsService {
@@ -36,17 +32,19 @@ public class GoogleEventsService {
         this.netHttpTransport = netHttpTransport;
     }
 
-    @Async
-    public CompletableFuture<List<Event>> getGoogleEvents() {
-
-
-        googleAuthorizationCodeFlow.n
-    }
+//    @Async
+//    public CompletableFuture<List<Event>> getGoogleEvents() {
+//
+//
+//        googleAuthorizationCodeFlow.n
+//    }
 
     private Calendar buildCalendar(CustomerPrinciple currentUser) throws IOException {
         Credential credential = googleAuthorizationCodeFlow.loadCredential(currentUser.getUserID());
         Calendar clientCalendar = new Calendar.Builder(netHttpTransport, jsonFactory, credential)
                 .setApplicationName(applicationName)
                 .build();
+
+        return clientCalendar;
     }
 }
