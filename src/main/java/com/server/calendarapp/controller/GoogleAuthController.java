a414f768a69b6b5b65c09898e5423562fdc4a3ef
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2019-12-17 19:18:07 +0100

    update google auth controller

--- src/main/java/com/server/calendarapp/controller/GoogleAuthController.java
+++ src/main/java/com/server/calendarapp/controller/GoogleAuthController.java
@@ -3,7 +3,6 @@ package com.server.calendarapp.controller;
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
 import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.server.calendarapp.exception.GoogleLoginException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,22 +31,25 @@ public class GoogleAuthController {
         this.googleAuthorizationCodeRequestUrl = googleAuthorizationCodeRequestUrl;
     }
 
-    @ExceptionHandler(GoogleLoginException.class)
     @GetMapping("/login")
     public void googleLogin(HttpServletResponse response) throws IOException {
         response.sendRedirect(googleAuthorizationCodeRequestUrl.build());
     }
 
 
-    @ExceptionHandler(GoogleLoginException.class)
-    @GetMapping("/login/callback")
-    public void googleLoginCallback(@RequestParam(value = "code") String code) throws IOException {
+    @PostMapping("/login/callback")
+    public void googleLoginCallback(@RequestParam(value = "code") String code) {
 
-        TokenResponse response = googleAuthorizationCodeFlow
-                .newTokenRequest(code)
-                .setRedirectUri(redirectURI)
-                .execute();
-        googleAuthorizationCodeFlow.createAndStoreCredential(response, "userID");
+        try {
+            TokenResponse response = googleAuthorizationCodeFlow
+                    .newTokenRequest(code)
+                    .setRedirectUri(redirectURI)
+                    .execute();
+            googleAuthorizationCodeFlow.createAndStoreCredential(response, "userID");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
 
     }
 
