0a63d670541a8acca6293acf1a05497d4c525c4e
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2019-12-18 13:09:24 +0100

    fix google auth

--- src/main/java/com/server/calendarapp/controller/GoogleAuthController.java
+++ src/main/java/com/server/calendarapp/controller/GoogleAuthController.java
@@ -3,21 +3,16 @@ package com.server.calendarapp.controller;
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
 import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.server.calendarapp.payload.ApiResponse;
-import com.server.calendarapp.payload.GoogleLoginResponse;
 import com.server.calendarapp.security.CurrentUser;
 import com.server.calendarapp.security.CustomerPrinciple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth/google")
@@ -39,24 +34,27 @@ public class GoogleAuthController {
         this.googleAuthorizationCodeRequestUrl = googleAuthorizationCodeRequestUrl;
     }
 
-    @GetMapping("/login")
-    public ResponseEntity<?> googleLogin(HttpServletResponse response, @CurrentUser CustomerPrinciple currentUser) throws IOException {
-        System.out.println(googleAuthorizationCodeRequestUrl.setState(currentUser.getUserID()).build());
-        return ResponseEntity.ok().body(new GoogleLoginResponse(googleAuthorizationCodeRequestUrl.setState(currentUser.getUserID()).build()));
+//    @GetMapping("/login")
+//    public ResponseEntity<?> googleLogin(HttpServletResponse response, @CurrentUser CustomerPrinciple currentUser) throws IOException {
+//        System.out.println(googleAuthorizationCodeRequestUrl.setState(currentUser.getUserID()).build());
+//        return ResponseEntity.ok().body(new GoogleLoginResponse(googleAuthorizationCodeRequestUrl.setState(currentUser.getUserID()).build()));
+//
+//    }
 
-    }
 
+    @PostMapping("/login/callback")
+    public ResponseEntity<?> googleLoginCallback(@RequestParam(value = "code") String code, @CurrentUser CustomerPrinciple currentUser) {
 
-    @GetMapping("/login/callback")
-    public ResponseEntity<?> googleLoginCallback(@RequestParam(value = "code") String code,
-                                                 @RequestParam(value = "state") String state) throws IOException {
+        try {
+            TokenResponse response = googleAuthorizationCodeFlow.newTokenRequest(code)
+                    .setRedirectUri(redirectURI)
+                    .execute();
+            googleAuthorizationCodeFlow.createAndStoreCredential(response, currentUser.getUserID());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        TokenResponse response = googleAuthorizationCodeFlow.newTokenRequest(code)
-                .setRedirectUri(redirectURI)
-                .execute();
-        googleAuthorizationCodeFlow.createAndStoreCredential(response, state);
-
-        return ResponseEntity.ok().body(new ApiResponse(true, "You have succesfully logged in"));
+        return ResponseEntity.ok().build();
     }
 
 
