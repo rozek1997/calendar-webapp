aaf3d725e78f59d2ce51b79c24b95dd9d6d34316
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2019-12-18 13:32:42 +0100

    fix security

--- src/main/java/com/server/calendarapp/config/SecurityConfig.java
+++ src/main/java/com/server/calendarapp/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/*", "/auth/google/login/callback").permitAll()
+                .antMatchers("/api/auth/*").permitAll()
                 .anyRequest()
                 .authenticated();
 
